df73069d66977d22d356bd1452309f08354367c9
spitgranger <richard.fan2000@hotmail.com>
2023-01-08 19:15:53 -0500

    updated features

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -3,10 +3,40 @@ import PostMessage from '../models/postMessage.js'
 
 
 export const getPosts = async (req, res) => {
+    const {page} = req.query;
     try {
-        const postMessages = await PostMessage.find();
-        console.log(postMessages);
-        res.status(200).json(postMessages);
+        const LIMIT = 8;
+        const startIndex = (Number(page) -1) * 8 //starting inde of every page
+        const total = await PostMessage.countDocuments({});
+
+        const posts= await PostMessage.find().sort({_id: -1}).limit(LIMIT).skip(startIndex);
+        res.status(200).json({data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total/LIMIT)});
+    } catch (error) {
+        console.log(error)
+        res.status(404).json( {message: error.message} )
+    }
+}
+
+export const getPost = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const post = await PostMessage.findById(id);
+        console.log(post)
+
+        res.status(200).json(post);
+    } catch (error) {
+        console.log(error)
+        res.status(404).json( {message: error.message} )
+    }
+}
+
+
+export const getPostsBySearch = async (req, res) => {
+    const {searchQuery, tags} = req.query
+    try {
+        const title = RegExp(searchQuery, 'i')
+        const posts = await PostMessage.find({ $or: [ {title}, {tags: {$in: tags.split(',')}} ] })
+        res.json({data: posts})
     } catch (error) {
         res.status(404).json( {message: error.message} )
     }
